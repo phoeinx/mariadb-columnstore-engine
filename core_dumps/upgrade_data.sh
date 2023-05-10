f60989f38fa3cc0238098efa25cf11bde894f1a2
mariadb-AndreyPiskunov <andrey.piskunov@mariadb.com>
2023-05-10 15:09:34 +0300

    fix script

--- core_dumps/upgrade_data.sh
+++ core_dumps/upgrade_data.sh
@@ -14,9 +14,9 @@ get_data ()
 {
   NAME=$1
   if curl -o "${SCHEMA_DIR}/${NAME}.csv" -# "https://sample-columnstore-data.s3.us-west-2.amazonaws.com/${NAME}.csv"; then
-     echo -e "Downloaded '${NAME}.csv' ... ${GREEN}done${NC}\n"
+     echo -e "Downloaded '${NAME}.csv' ... done\n"
   else
-     echo -e "Downloading '${NAME}.csv' ... ${RED}failed${NC}"
+     echo -e "Downloading '${NAME}.csv' ... failed"
      exit 1
   fi
 }
@@ -26,16 +26,16 @@ import_data ()
     NAME=$1
     echo -e "\nLoading '${NAME}.csv' with cpimport ..."
     if ! $CPIMPORT -m 1 -s ',' -E '"' columnstore_bts "${NAME}" -l "${SCHEMA_DIR}/${NAME}.csv"; then
-        echo -e "loading '${NAME}.csv' ... ${RED}fail${NC}"
+        echo -e "loading '${NAME}.csv' ... fail"
         exit 1
     fi
 }
 
 
 if $MARIADB <"${SCHEMA_DIR}"/columnstore_schema.sql &>/dev/null; then
-    echo -e "Creating 'columnstore_bts' schema..." "${GREEN}done${NC}"
+    echo -e "Creating 'columnstore_bts' schema..." "done"
 else
-    echo -e "Creating 'columnstore_bts' schema..." "${RED}fail${NC}"
+    echo -e "Creating 'columnstore_bts' schema..." "fail"
     exit 1
 fi
 
