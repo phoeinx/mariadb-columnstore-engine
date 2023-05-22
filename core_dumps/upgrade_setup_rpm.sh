e17836ef30fdd3955884e466db9b0625c8c1549e
mariadb-AndreyPiskunov <andrey.piskunov@mariadb.com>
2023-05-22 16:20:16 +0300

    Add -y

--- core_dumps/upgrade_setup_rpm.sh
+++ core_dumps/upgrade_setup_rpm.sh
@@ -8,7 +8,7 @@ ARCH="$3"
 UPGRADE_TOKEN="$4"
 
 dnf install -y wget which procps-ng
-yum install diffutils
+yum install -y diffutils
 wget https://dlm.mariadb.com/enterprise-release-helpers/mariadb_es_repo_setup -O mariadb_es_repo_setup
 chmod +x mariadb_es_repo_setup
 bash -c "./mariadb_es_repo_setup --token=${UPGRADE_TOKEN} --apply --mariadb-server-version=${VERSION} --skip-maxscale --skip-tools"
